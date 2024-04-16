30e156d6b6c9df8a5da0050533323ad00a7e0225
lenzLLL <lenzyounda@example.com>
2024-04-16 13:30:39 +0100

    first commit

---
+++ controllers/emailControllers.js
@@ -0,0 +1,39 @@
+const expressAsyncHandler = require("express-async-handler");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+dotenv.config();
+
+let transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  tls: {
+    rejectUnauthorized: false
+  }, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_MAIL, // generated ethereal user
+    pass: process.env.SMTP_PASSWORD, // generated ethereal password
+  },
+});
+
+const sendEmail = expressAsyncHandler(async (req, res) => {
+  const { email, subject, message } = req.body;
+  console.log(email, subject, message);
+
+  var mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject: subject,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json({msg:"email sent succesfully"})
+    }
+  });
+});
+
+module.exports = { sendEmail };
